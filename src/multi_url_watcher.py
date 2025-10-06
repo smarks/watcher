7917cad65f47946fe4d11f1ecaaff4a9b3c3844f
Spencer <spencer.marks@childrens.harvard.edu>
2025-10-06 17:31:15 -0400

    updates

--- src/multi_url_watcher.py
+++ src/multi_url_watcher.py
@@ -20,8 +20,12 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-from src.clicksend_sms_notifier import ClickSendSMSNotifier as SMSNotifier
-from src.clicksend_sms_notifier import create_notifier_from_env
+try:
+    from src.clicksend_sms_notifier import ClickSendSMSNotifier as SMSNotifier
+    from src.clicksend_sms_notifier import create_notifier_from_env
+except ImportError:
+    from clicksend_sms_notifier import ClickSendSMSNotifier as SMSNotifier
+    from clicksend_sms_notifier import create_notifier_from_env
 
 
 class ResilientURLWatcher:
